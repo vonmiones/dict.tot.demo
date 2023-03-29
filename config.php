13a9d55070397441fa7991b28a95a99e3c137b27
vonmiones <vonmiones@gmail.com>
2023-03-30 05:11:42 +0800

    main

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+$config["title"] = "";
+
+$config["dbhost"] = "";
+$config["dbport"] = "";
+$config["dbname"] = "";
+$config["dbuser"] = "";
+$config["dbpass"] = "";
+
+$config["theme-default"] = "default";
+$config["libs"] = "";
+$config["apps"] = "";
+
+$config["logs"] = "";
--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DICT - Training of Traners Demo Site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='assets/css/main.css' rel='stylesheet'>
+    <link href='assets/css/fonts.css' rel='stylesheet'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" integrity="sha512-2bMhOkE/ACz21dJT8zBOMgMecNxx0d37NND803ExktKiKdSzdwn+L7i9fdccw/3V06gM/DBWKbYmQvKMdAA9Nw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
-    Hello World
+    <div class="container">
+        <div class="flexr wrapper">
+            <div class="sidebar bg-color">
+                <h1>Team Asenso</h1>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="content"></div>
+        </div>
+    </div>
 </body>
 </html>
